249cf003b525fabd29e778f2543e42f0a6d01976
Jesús Mejía <jesusmejded@gmail.com>
2018-09-01 09:32:21 -0500

    testing new methos

--- test.jl
+++ test.jl
@@ -3,7 +3,9 @@ include("optimizer.jl")
 function test()
     f(x) = x^2 + 54 / x
 
-    exhaustiveSearch(f, 0, 5, 10)
+    exhaustiveSearch(f, 0, 5, 2000)
+    boundingPhase(f, 0.6, 0.5)
+    intervalHalving(f, 0, 5, 1e-3)
 end
 
 test()
