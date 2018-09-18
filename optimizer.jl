d76f6365c4e4f84c4d6f31aec8b75afdbac198b5
Jesus Mejia <jesusmejded@gmail.com>
2018-09-18 02:19:28 -0500

    powell

--- optimizer.jl
+++ optimizer.jl
@@ -186,3 +186,55 @@ function goldenSection(f::Function, a::Real, b::Real, ε::Float64; debug::Bool=f
 
     return aw*(b-a) + a, bw*(b-a) + a
 end
+
+function powell(f::Function, x::Real, Δ::Real, TOL1::Real, TOL2::Real; debug::Bool=false)
+
+    x1 = x
+    x2 = x1 + Δ
+
+    f1, f2 = f(x1), f(x2)
+    
+    if f1 > f2
+        x3 = x1 + 2Δ
+    else
+        x3 = x1 - 2Δ
+    end
+
+    f3 = f(x3)
+
+    Fs = Real[f1, f2, f3]
+    Xs = Real[x1, x2, x3]
+
+    while true
+        i = argmin(Fs)
+        X_min = Xs[i]
+        F_min = Fs[i]
+
+
+        a1 = (f2 - f1)/(x2- x1)
+        a2 = ( (f3 - f1)/(x3 - x1) - a1 ) / (x3-x2)
+        
+        xb = 0.5*(x1 + x2) - a1/(2*a2)
+        fb = f(xb)
+
+
+        push!(Xs, xb)
+        push!(Fs, fb)
+
+        if abs(F_min - fb) <= TOL1 && abs(X_min - xb) <= TOL1
+            return Xs[argmin(Fs)]
+        end
+
+        j = sortperm( Fs )[1:3]
+        Xs = Xs[ j ]
+        Fs = Fs[ j ]
+
+        j = sortperm( Xs )[1:3]
+
+        x1, x2, x3 = Xs[j]
+        f1, f2, f3 = Fs[j]
+        
+        debug && @printf("x = %.4f \t f = %.4f\n", X_min, F_min)
+
+    end
+end
